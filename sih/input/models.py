adbb569d78e281cc433528035b7b8e45db02a8b8
Govind singh <rgovindsingh51@gmail.com>
2019-03-02 15:53:58 +0530

    support added of .gz file

--- sih/input/models.py
+++ sih/input/models.py
@@ -1,6 +1,11 @@
+import os
 from django.db import models
 
 # Create your models here.
 
 class InputImage(models.Model):
 	image = models.FileField(upload_to='input/')
+
+	def extension(self):
+		exten = os.path.splitext(self.image.name)
+		return exten[1]
--- sih/input/views.py
+++ sih/input/views.py
@@ -1,9 +1,13 @@
+import os
+from django.core.files.base import File
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import FormView, DetailView
 from .models import InputImage
 from .forms import InputImageForm
+import gzip
+import shutil
 
 class InputImageView(FormView):
     template_name = 'image_upload.html'
@@ -12,6 +16,20 @@ class InputImageView(FormView):
     def form_valid(self, form):
         input_image = InputImage(
             image=self.get_form_kwargs().get('files')['image'])
+        input_image.save()
+        self.id = input_image.id
+
+        s = input_image.image.path
+        s = s[0:-3]
+
+        if(input_image.extension() == ".gz"):
+            with gzip.open(input_image.image.path,'rb') as f_in:
+                with open(s,'wb') as f_out:
+                    shutil.copyfileobj(f_in,f_out)
+
+        s = s[58:]
+
+        input_image = InputImage( image = s)
         input_image.save()
         self.id = input_image.id
 
